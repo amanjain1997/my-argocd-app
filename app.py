a5ff491d8d1526251438e29e8921180f53f8be3a
amanjain1997 <42401106+amanjain1997@users.noreply.github.com>
2025-07-19 19:50:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from ArgoCD!"
+    return "Hello from ArgoCD! Aman"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
