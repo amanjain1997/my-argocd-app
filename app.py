90188a42757d00761ad3d8884840d84098391f43
Aman Jain <aman.jain@TC-L0046s-MacBook-Pro.local>
2025-07-19 21:10:41 +0530

    WIP: saving changes before pulling

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
+<<<<<<< HEAD
     return "Hello from ArgoCD! Aman"
+=======
+    return "Hello from ArgoCD! Amisha"
+>>>>>>> 28db487 (WIP: saving changes before pulling)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
