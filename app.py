1f6f30257fcd51cfb057342d9c030953a46d568d
amanjain1997 <42401106+amanjain1997@users.noreply.github.com>
2025-07-19 21:19:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-<<<<<<< HEAD
-    return "Hello from ArgoCD! Aman"
-=======
-    return "Hello from ArgoCD! Amisha"
->>>>>>> 28db487 (WIP: saving changes before pulling)
+    return "Hello from ArgoCD! Aman"  # Or use Amisha — your choice
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
-
